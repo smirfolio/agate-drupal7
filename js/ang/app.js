0af69acc3532b68bbdcab8d4f95c6c6bd5aacee4
smirfolio <ben.sghaier.samir@gmail.com>
2015-05-15 13:06:12 -0400

    AGATE-39 obiba_auth: form for user sign up

---
+++ js/ang/app.js
@@ -0,0 +1,81 @@
+/**
+ * @file
+ * JavaScript ajax helper for Statistics variables retrieving
+ */
+(function ($) {
+  Drupal.behaviors.obiba_auth = {
+    attach: function (context, settings) {
+      'use strict';
+      /* App Module */
+      var obibaAth = angular.module('ObibaAuth', [
+//        'ngAnimate',
+//        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.bootstrap',
+//        'ngRoute',
+        'schemaForm'
+      ]);
+
+      obibaAth.controller('RegisterFormController', ['$scope', '$log', 'UserResource', function ($scope, $log, UserResource) {
+
+        settings.form.push(
+          {
+            type: "submit",
+            title: Drupal.t('Join')
+          }
+        );
+
+        $scope.form = angular.fromJson(settings.form);
+        $scope.schema = angular.fromJson(settings.schema);
+        $scope.model = {};
+
+        $scope.onSubmit = function (form) {
+          // First we broadcast an event so all fields validate themselves
+          $scope.$broadcast('schemaFormValidate');
+          // Then we check if the form is valid
+          if (form.$valid) {
+            UserResource.post($scope.model).
+              success(function (data, status, headers, config) {
+                $scope.alert = {
+                  message: Drupal.t('You will receive an email to confirm your registration.'),
+                  type: 'success'
+                };
+                var elem = document.getElementById("obiba-user-register");
+                angular.element(elem).remove();
+
+              })
+              .error(function (data, status, headers, config) {
+                var errorParse = angular.fromJson(data);
+                console.log(errorParse);
+                $scope.alert = {
+                  message: Drupal.t(' Code :' + status + ' :' + errorParse.message),
+                  type: 'danger'
+                };
+              });
+          }
+          $scope.closeAlert = function () {
+            $scope.alert = [];
+          };
+        };
+
+      }]);
+
+
+      obibaAth.factory('UserResource', ['$http',
+        function ($http) {
+          return {
+            post: function (data) {
+              return $http.post('/drupal/agate_user_join', $.param(data), {
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+              });
+            }
+          };
+        }]);
+
+
+    }
+  }
+}(jQuery));
+
+
---
+++ templates/obiba_auth_user-register-form.tpl.php
@@ -0,0 +1,25 @@
+<?php
+//dpm($variables);
+?>
+
+<div class="outer">
+    <div class="innerdivs">
+        <div class="row">
+            <div class="col-md-6 text-center "><?php print l('Login', 'user/login') ?></div>
+            <div class="col-md-6 text-center bg-primary"><?php print t('Create an account') ?></div>
+        </div>
+
+        <div class="obiba-bootstrap-user-register-form-wrapper">
+
+            <div ng-app="ObibaAuth" ng-controller="RegisterFormController">
+
+                <alert ng-if="alert.message" type="{{alert.type}}" close="closeAlert($index)">{{alert.message}}</alert>
+
+                <form id="obiba-user-register" name="theForm" ng-submit="onSubmit(theForm)">
+                    <div sf-schema="schema" sf-form="form" sf-model="model"></div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
