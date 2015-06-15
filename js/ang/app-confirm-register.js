8e6e8c7efa48efb6e3f97c6b314f04d1986cbb70
smirfolio <ben.sghaier.samir@gmail.com>
2015-06-15 15:20:43 -0400

    AGATE-39 Debug og reset password + confirm password

--- js/ang/app-confirm-register.js
+++ js/ang/app-confirm-register.js
@@ -6,10 +6,11 @@
   Drupal.behaviors.obiba_agate_confirm_register = {
     attach: function (context, settings) {
 
+      var resource = location.href.split('#');
       var query = location.href.split('=');
+      var resourceAction = /\/(.*?)\?key/i.exec(resource[1]);
       $('#key').val(query[1]);
       $('#verif-password').keyup(function(){
-
         if($('#type-password').val() != $('#verif-password').val()) {
           $('.form-item-repassword').addClass('has-error');
         }
@@ -26,16 +27,17 @@
           return false;
         }
         else{
-          $.ajax({
-            method: "POST",
-            url: Drupal.settings.basePath + "agate/send_confirmation",
-            data: { key: query[1], password:$('#type-password').val() }
-          })
-            .done(function( msg ) {
+            ajaxrequest = $.ajax({
+              method: "POST",
+              url: Drupal.settings.basePath + "agate/send_password/"+resourceAction[1],
+              data: { key: query[1], password:$('#type-password').val() }
+            });
+
+          ajaxrequest.done(function( msg ) {
               window.location = Drupal.settings.basePath + 'user/login';
               return false;
-            })
-            .fail(function(){
+            });
+          ajaxrequest.fail(function(){
               console.log('error');
               window.location = Drupal.settings.basePath;
               return false;
